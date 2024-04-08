cdf264961798cb518cab57af8ce2b3c9c6b59dc3
HANS ELIJAH VIDUYA <2233053@slu.edu.ph>
2024-04-08 12:28:10 +0800

    initial frame

---
+++ src/AppLauncher.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Main();
+        });
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+
+    public Main(){
+        setTitle("Word Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setSize(650,500);
+        setVisible(true);
+    }
 }
