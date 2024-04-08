0541e9c1fb5f5827f21e65d27143061e2c609526
HANS ELIJAH VIDUYA <2233053@slu.edu.ph>
2024-04-08 13:22:22 +0800

    import libraries

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class AppLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            new Main();
+            new WordGameGUI().setVisible(true);
         });
     }
 }
