4e05a4383dce9c047c3565ecf85139befe045033
NEIL ANGELO BRIONES <2233376@slu.edu.ph>
2024-04-18 12:53:23 +0800

    add custom exceptiona and update .gitignore rules

---
+++ src/exceptions/NotLoggedInException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+/**
+ *  Creates a throwable when the user is not logged in.
+ */
+public class NotLoggedInException extends Exception {
+
+    /**
+     *
+     * @param message error message.
+     */
+    public NotLoggedInException(String message) {
+        super(message);
+    }
+}
