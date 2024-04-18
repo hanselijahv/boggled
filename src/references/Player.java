ecb181bcff28404a4133fbf62437a174606e4fff
MATT DANIELLE BRAVO <2233368@slu.edu.ph>
2024-04-18 12:53:23 +0800

    Player and Leaderboard reference classes

---
+++ src/references/Player.java
@@ -0,0 +1,31 @@
+package references;
+
+public class Player {
+    private String username;
+    private String password;
+
+    public Player(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return "Username: " + username + "\nPassword: " + password;
+    }
+}
