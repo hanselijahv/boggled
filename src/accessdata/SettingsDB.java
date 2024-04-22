5395887a6813facc843a95076407c9ceac76fc29
BRYAN PASCUAL <2233130@slu.edu.ph>
2024-04-22 17:40:46 +0800

    SettingsDB.java updateSettings Method

--- src/accessdata/SettingsDB.java
+++ src/accessdata/SettingsDB.java
@@ -1,9 +1,29 @@
 package accessdata;
 
+
 import references.Settings;
 import java.sql.*;
 
 
 public class SettingsDB {
     //TODO Bryan
+
+    private Connection connection;
+
+    public SettingsDB(){
+        this.connection = DatabaseConnector.getInstance().getConnection();
+    }
+
+    public void updateSettings(Settings settings){ // needs testing
+        String query = "UPDATE settings SET waitingTime = ?, roundTime = ?, numberOfRounds = ?";
+        try(PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setTimestamp(1,settings.getWaitingTime());
+            statement.setTimestamp(2,settings.getRoundTime());
+            statement.setInt(3,settings.getNumberOfRounds());
+            statement.execute();
+            System.out.println("Saved Settings...");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
