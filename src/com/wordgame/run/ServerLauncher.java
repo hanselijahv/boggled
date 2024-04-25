3c08431c8faf39b2737bf7f80db61cf67eac48fc
HANS ELIJAH VIDUYA <2233053@slu.edu.ph>
2024-04-25 13:16:30 +0800

    created server frame DevDisplay.java

---
+++ src/com/wordgame/run/ServerLauncher.java
@@ -0,0 +1,11 @@
+package com.wordgame.run;
+
+import com.wordgame.boggled.server.DevDisplay;
+
+import javax.swing.*;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+        new DevDisplay(1280, 720);
+    }
+}
