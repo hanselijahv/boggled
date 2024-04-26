8935250afaf218a9b5761d4510b757d2413d5693
NEIL ANGELO BRIONES <2233376@slu.edu.ph>
2024-04-26 23:19:08 +0800

    implement partial DAO for Settings Table

--- src/com/wordgame/common/db/SettingsDAO.java
+++ src/com/wordgame/common/db/SettingsDAO.java
@@ -3,10 +3,13 @@ package com.wordgame.common.db;
 
 import com.wordgame.common.model.Settings;
 import java.sql.*;
+import java.util.Collections;
+import java.util.List;
 
-
-public class SettingsDAO {
-    //TODO Bryan
+/**
+ *
+ */
+public class SettingsDAO implements DAO<Settings> {
 
     private final Connection connection;
 
@@ -14,18 +17,32 @@ public class SettingsDAO {
         this.connection = DatabaseConnector.getInstance().getConnection();
     }
 
-    public void updateSettings(Settings settings){ // needs testing
+    @Override
+    public List<Settings> getAll() throws SQLException {
+        return Collections.emptyList(); // TODO: Implement
+    }
+
+    @Override
+    public boolean insert(Settings settings) throws SQLException {
+        return false;   // TODO: Implement
+    }
+
+    // TODO: Resolve missing reference for setting
+    @Override
+    public boolean update(Settings settings, String[] params) throws SQLException {
         String query = "UPDATE settings SET waiting_time = ?, round_time = ?, number_of_rounds = ?";
         try(PreparedStatement statement = connection.prepareStatement(query)){
             statement.setInt(1,settings.getWaitingTime());
             statement.setInt(2,settings.getRoundTime());
             statement.setInt(3,settings.getRoundsToWin());
-            statement.execute();
-            System.out.println("Saved Settings...");
+            return statement.execute();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            throw new SQLException("[ERROR] Failed to update settings: " + e.getMessage());
         }
     }
 
-
+    @Override
+    public boolean delete(Settings settings) throws SQLException {
+        return false;   // TODO: Implement
+    }
 }
