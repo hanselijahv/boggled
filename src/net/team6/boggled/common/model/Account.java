808b92264895e8fead0d72a02ac551d5713f10cd
NEIL ANGELO BRIONES <2233376@slu.edu.ph>
2024-05-06 13:25:33 +0800

    update idl

--- src/net/team6/boggled/common/model/Account.java
+++ src/net/team6/boggled/common/model/Account.java
@@ -5,6 +5,11 @@ public class Account {
     private String username;
     private String password;
 
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public Account(String playerId, String username, String password) {
         this.playerId = playerId;
         this.username = username;
---
+++ src/net/team6/boggled/common/model/RoundWinner.java
@@ -0,0 +1,7 @@
+package net.team6.boggled.common.model;
+
+public class RoundWinner {
+    private String gameID;
+    private String playerID;
+    private int roundScore;
+}
